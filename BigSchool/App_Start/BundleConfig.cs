bd85d212b2bdc61e8b0b11189032e2fd54e0452f
Tri <Dell@DESKTOP-D44L2SV>
2019-04-11 19:17:01 +0700

    66

--- BigSchool/App_Start/BundleConfig.cs
+++ BigSchool/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace BigSchool
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/app/coursesController.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,7 +22,9 @@ namespace BigSchool
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
